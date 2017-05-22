02b247fe9837693f4ae5eab1e36159ece8c1324b
sarang katare <sarang.katare@yahoo.com>
2017-05-22 01:15:10 -0700

    insatallled coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp'),
-    gutil= require('gulp-util');
+    gutil= require('gulp-util'),
+    coffee=require('gulp-coffee');
 
-    gulp.task('log',function(){
+    gulp.task('coffee',function(){
+      gulp.src('component/coffee/tagline.coffee')
+          .pipe(coffee({bare:true})
+          .on('error',gutil.log))
+          .pipe(gulp.dest('component/scripts'))
 
-      gutil.log('workflows are awesome');
     });
